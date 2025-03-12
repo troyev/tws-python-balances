d06e5d88f97966a820c767904f4ebc4f2ba3b174
Troy Velius <troyvelius@gmail.com>
2025-03-11 22:39:21 -0400

    launch

--- launch.bat
+++ launch.bat
@@ -1,7 +1,7 @@
+@echo off
 REM to make this launchable from the taskbar
 REM make a shortcut, then add cmd /c at the beginning
-REM then pin to taskbar
-@echo off
+REM then pin to taskbar.
 cd /d "C:\Users\troy\tws-python-balances\venv\Scripts"
 call activate
 cd /d "C:\Users\troy\tws-python-balances"
